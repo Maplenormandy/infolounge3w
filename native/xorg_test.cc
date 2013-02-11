0c1188b2c595c01adde6cc8b12cc13b1fad2e43e
Norman Cao <maplenormandy@gmail.com>
2013-02-10 23:54:20 -0500

    Checks window position
    (I can't compile on windows so I need to push constantly)
    
    Signed-off-by: Norman Cao <maplenormandy@gmail.com>

--- native/xorg_test.cc
+++ native/xorg_test.cc
@@ -11,38 +11,60 @@ Display *displayMain;
 
 void terminate(int param)
 {
-	cout << "Exiting..." << endl;
-	
-	if (displayMain != NULL)
-		XCloseDisplay(displayMain);
-	
-	exit(EXIT_SUCCESS);
+    cout << "Exiting..." << endl;
+    
+    if (displayMain != NULL)
+        XCloseDisplay(displayMain);
+    
+    exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv)
 {
-	displayMain = XOpenDisplay(NULL);
-	
-	if(displayMain == NULL)
-	{
-		cerr << "Can't find a main display!" << endl;
-		exit(EXIT_FAILURE);
-	}
-	
-	void (*prev_fn)(int);
-	
-	prev_fn = signal (SIGTERM,terminate);
-	if (prev_fn==SIG_IGN) signal (SIGTERM,SIG_IGN);
-	
-	while (true)
-	{
-		int x, y;
-		
-		cin >> x;
-		cin >> y;
-		
-		XWarpPointer(displayMain, None, None, 0, 0, 0, 0, x, y);
-	}
-	
-	return 0;
+    displayMain = XOpenDisplay(NULL);
+    
+    if(displayMain == NULL)
+    {
+        cerr << "Can't find a main display!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    void (*prev_fn)(int);
+    
+    prev_fn = signal (SIGTERM,terminate);
+    if (prev_fn==SIG_IGN) signal (SIGTERM,SIG_IGN);
+    
+    number_of_screens = XScreenCount(display);
+    fprintf(stderr, "There are %d screens available in this X session\n", number_of_screens);
+    root_windows = malloc(sizeof(Window) * number_of_screens);
+    for (i = 0; i < number_of_screens; i++) {
+        root_windows[i] = XRootWindow(display, i);
+    }
+    for (i = 0; i < number_of_screens; i++) {
+        result = XQueryPointer(display, root_windows[i], &window_returned,
+                &window_returned, &root_x, &root_y, &win_x, &win_y,
+                &mask_return);
+        if (result == True) {
+            break;
+        }
+    }
+    if (result != True) {
+        fprintf(stderr, "No mouse found.\n");
+        return -1;
+    }
+    printf("Mouse is at (%d,%d)\n", root_x, root_y);
+
+    free(root_windows);
+    
+    while (true)
+    {
+        int x, y;
+        
+        cin >> x;
+        cin >> y;
+        
+        XWarpPointer(displayMain, None, None, 0, 0, 0, 0, x, y);
+    }
+    
+    return 0;
 }
