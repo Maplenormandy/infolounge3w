8b431babbfd2f96490546b3025318f3bbb9b9358
Norman Cao <maplenormandy@gmail.com>
2013-02-10 23:32:22 -0500

    Added xorg test code
    Console input to move mouse. No networking yet.
    
    Signed-off-by: Norman Cao <maplenormandy@gmail.com>

---
+++ native/xorg_test.cc
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+using namespace std;
+
+Display *displayMain;
+
+void mouseMove(int x, int y)
+{
+	
+	
+	
+	
+	
+}
+
+void terminate(int param)
+{
+	cout << "Exiting..." << endl;
+	
+	if (displayMain != NULL)
+		XCloseDisplay(displayMain);
+	
+	exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+	displayMain = XOpenDisplay(NULL);
+	
+	if(displayMain == NULL)
+	{
+		cerr << "Can't find a main display!" << endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	void (*prev_fn)(int);
+	
+	prev_fn = signal (SIGTERM,terminate);
+	if (prev_fn==SIG_IGN) signal (SIGTERM,SIG_IGN);
+	
+	while (true)
+	{
+		int x, y;
+		
+		cin >> x;
+		cin >> y;
+		
+		XWarpPointer(displayMain, None, None, 0, 0, 0, 0, x, y);
+	}
+	
+	return 0;
+}
