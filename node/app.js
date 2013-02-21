47560e2b25c22d5015e297ebdd4840cae3a0cb96
Norman Cao <maplenormandy@gmail.com>
2013-02-21 23:37:58 +0000

    Added legacy infolounge

--- node/app.js
+++ node/app.js
@@ -30,6 +30,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/legacy', routes.legacy);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- node/routes/index.js
+++ node/routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.legacy = function(req, res){
+  res.sendfile('views/legacy.html');
+};
